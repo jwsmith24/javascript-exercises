e0c672508a1d158f55b3ed3c02d870570d89e459
Jake <jsmith181997@gmail.com>
2024-02-27 18:40:13 -1000

    Implement reverse string methodto pass all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,29 @@
-const reverseString = function() {
+const reverseString = function (string) {
+
+    if (string === "") {
+        return "";
+    }
+
+    let letters = [];
+
+    // read in the chars
+    for (let i = 0; i < string.length; i++) {
+        letters.push(string[i]);
+    }
+
+    let reverseString = letters.pop();
+
+    let letterCount = letters.length;
+
+    // pop the letters off to get the reverse
+    for (let index = 0; index < letterCount; index++) {
+        reverseString = reverseString + letters.pop();
+    }
+
+    return reverseString;
+
+
+
 
 };
 
