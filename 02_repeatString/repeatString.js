6191f91bf625b7665e2a166b8090bbbe23e0a44c
Jake <jsmith181997@gmail.com>
2024-02-27 18:23:55 -1000

    Implement stringRepeat to pass all provided test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,27 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+
+    // check error conditions
+    if (number < 0) {
+        return "ERROR";
+
+    } else if (number == 0 || string === "") {
+        return "";
+
+    } else {
+        // start the chain (accounted for in loop iterations)
+        let result = string;
+
+        for (let i = 0; i < number - 1; i++) {
+            // cat the word onto existing chain
+            result = result + string;
+        }
+
+        return result;
+
+    }
+
+
+
 
 };
 
