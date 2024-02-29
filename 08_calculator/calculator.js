f89d4edb61adb3b1be2408fdf8b17b4ba125e2bf
Jake <jsmith181997@gmail.com>
2024-02-29 12:24:41 -1000

    Compelte calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, number) => sum += number, 0);
+}
+
+
+const multiply = function (array) {
+  return array.reduce((total, number) => total = total * number, 1);
+}
+
+const power = function (number, exponent) {
+
+  let result = number;
+  for (let i = 1; i < exponent; i++) {
+
+    result = result * number;
+  }
+
+  return result;
+
+
 };
 
-const multiply = function() {
+const factorial = function (number) {
 
-};
+  // easy cases
+  if (number === 0) {
+    return 1;
+  }
 
-const power = function() {
-	
-};
+  let iterator = number - 1;
 
-const factorial = function() {
-	
+  let total = number;
+
+  while (iterator > 0) {
+
+    total = total * iterator;
+
+    iterator--;
+  }
+
+  return total;
+
+
 };
 
 // Do not edit below this line
